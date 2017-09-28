0d9c66c9c176b7afb2e02c77cf0119cbeb948f5e
BalazsBarna <balazsbarni@gmail.com>
2017-09-28 17:32:28 +0200

    feat(TodoList):Day ended

--- todo_main.py
+++ todo_main.py
@@ -4,28 +4,69 @@ import os
 
 class Todo(object):
     def __init__(self):
-        self.commands = [
+        os.system("clear")
+        self.arguments = [
             {"argument": "-l", "description": "Lists all the tasks"},
             {"argument": "-a", "description": "Adds new task"},
             {"argument": "-r", "description":"Removes a task"},
             {"argument": "-c", "description":"Completes a task"}
         ]
+        if len(sys.argv) == 2 and sys.argv[1] == "-l":
+            return self.list_todo()
+        elif len(sys.argv) == 3 and sys.argv[1] == "-a":
+            item = str(sys.argv[2])
+            return self.add_todo(item)
+        elif len(sys.argv) == 3 and sys.argv[1] == "-r":
+            index = int(sys.argv[2])
+            return self.remove_item(index)
+        elif len(sys.argv) == 3 and sys.argv[1] == "-c":
+            index = int(sys.argv[2])
+            return self.check_item(index)
+        else:
+            print ("Command Line Todo application\n" +
+            "=============================\n\n" +
+            "Command line arguments:")
+            for items in self.arguments:
+                print(items["argument"]+ " " + items["description"])
+    
+    
+    def add_todo(self, item):
+        file = open("todo_list.txt", "a") 
+        file.write(str(item + "\n")) 
+        file.close()
 
-    def commands_printer(self):
-        print ("Command Line Todo application\n" +
-        "=============================\n\n" +
-        "Command line arguments:")
-        for items in self.commands:
-            print(items["argument"]+ " " + items["description"])
+
+    def list_todo(self):
+        with open('todo_list.txt') as f:
+            lines = [line.rstrip('\n') for line in open('todo_list.txt')]
+            index_num = 1
+            for i in lines:
+                print(index_num, "[ ]" , i)
+                index_num +=1
 
 
-   
+    def remove_item(self, index):
+        with open("todo_list.txt","r") as textobj:
+            listed = list(textobj)
+            del listed[index - 1]    
+        with open("todo_list.txt","w") as textobj:
+            for n in listed:
+                textobj.write(n)
+
+    def check_item(self, index):
+        with open('todo_list.txt') as f:
+            lines = [line.rstrip('\n') for line in open('todo_list.txt')]
+            index_num = 1
+            if index == index_num:
+                print(index_num, "[X]" , i)
+            else:    
+                index_num +=1
+
 def start_todo():
-    os.system("clear")
     app = Todo()
-    args = sys.argv[1:]
-    if not args:
-        app.commands_printer()
+    
+    
+       
 
 
 start_todo()
